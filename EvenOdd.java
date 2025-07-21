5025518f9be70e57a5a0e507b7a8eb85fca08e4b
Khandakar Jarin Shova <jarinshova007@gmail.com>
2025-07-21 19:29:07 +0600

    A new file has been added

---
+++ EvenOdd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an integer number: ");
+        int num = sc.nextInt();
+
+        if(num % 2 == 0){
+            System.out.println("This is an Even number");
+        }
+        else{
+        System.out.println("This is an Odd number");
+        }
+}
+    
+}
