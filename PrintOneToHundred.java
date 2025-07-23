bac15070cabe95e89d035886284240f9d4637bdd
Khandakar Jarin Shova <jarinshova007@gmail.com>
2025-07-24 01:11:57 +0600

    feat: Add PrintOneToHundred program

---
+++ PrintOneToHundred.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class PrintOneToHundred {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the last number of the series : ");
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            System.out.print(i);
+            if (i < 100) {
+                System.out.print(", ");
+            }
+        }
+
+        sc.close();
+        
+    }
+}
+    
+
