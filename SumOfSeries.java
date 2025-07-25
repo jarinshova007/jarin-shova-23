5effb2215062eee3483c46b4ffadc14dc9963dfa
Khandakar Jarin Shova <jarinshova007@gmail.com>
2025-07-25 14:52:07 +0600

    style: Apply consistent code formatting

--- SumOfSeries.java
+++ SumOfSeries.java
@@ -4,7 +4,7 @@ public class SumOfSeries {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the last number of the series : ");
+        System.out.print("Enter the last number of the series: ");
         int num = sc.nextInt();
         
         int sum = 0;
