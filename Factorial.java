f0815b6ec4452ec23c2b776fb72717714efec3a1
Khandakar Jarin Shova <jarinshova007@gmail.com>
2025-07-25 15:23:11 +0600

    feat: Add Factorial program

---
+++ Factorial.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+    public long printFactorial(int num) {
+        long factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial = factorial * i; // factorial *= i;
+        }
+
+        System.out.println("The factorial of " + num + " is " + factorial);
+    //System.out.println(factorial);
+        return factorial;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an integer number: ");
+        int _num =  sc.nextInt();
+
+        if (_num < 0) {
+
+            System.out.println("Factorial is not defined for negative numbers");
+
+        } else {
+            Factorial f = new Factorial(); //making an object as the printFactorial method is non-static
+            f.printFactorial(_num);
+            
+            //System.out.println("The factorial of " + _num + " is " + _factorial);
+
+        }
+        
+        sc.close();
+
+    }
+}
+
+
